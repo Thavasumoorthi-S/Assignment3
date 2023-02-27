8334f0f1c0d4070cfc2de8c3c35682625efb3d52
Thavasumoorthi-S <thavasumoorthi.s@tringapps.com>
2023-02-27 21:58:25 +0530

    Third commit

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -2,53 +2,52 @@ package org.example;
 import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-class StudentException {
+class Student{
     private static final Logger LOGGER=Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
     String name;
     char grade;
     float gpa;
 
-    StudentException(String name,char grade,float gpa)
+    Student(String name,char grade,float gpa)
     {
         this.name=name;
         this.grade=grade;
         this.gpa=gpa;
     }
-    public void updategpa(float gpa)
+    public void updategpa(char grade)
     {
-        this.gpa=gpa;
-        if(gpa==6)
+        this.grade=grade;
+        if(this.grade=='D')
         {
-            grade='D';
+            gpa=6;
         }
-        else if(gpa==7)
+        else if(this.grade=='C')
         {
-            grade='C';
+            gpa=7;
         }
-        else if(gpa==8)
+        else if(this.grade=='B')
         {
-            grade='B';
+            gpa=8;
         }
-        else if(gpa==9)
+        else if(this.grade=='A')
         {
-            grade='A';
+            gpa=9;
         }
-        else if(gpa==10)
+        else if(this.grade=='O')
         {
-            grade='O';
+            gpa=10;
         }
     }
-    public float studentgpa()
+    public String studentdetails()
     {
-        return gpa;
+        return this.name+" has a "+this.gpa+" GPA";
     }
     public static void main(String[] args)
     {
         String name;
         char grade;
         float gpa;
-        float upgpa;
-        String res;
+        char upgrade;
         Scanner sc=new Scanner(System.in);
         LOGGER.log(Level.INFO,"Enter the Name: ");
         name=sc.nextLine();
@@ -56,21 +55,18 @@ class StudentException {
         grade=sc.next().charAt(0);
         LOGGER.log(Level.INFO,"Enter the GPA: ");
         gpa=sc.nextFloat();
-        StudentException s=new StudentException(name,grade,gpa);
+        Student s=new Student(name,grade,gpa);
         while(true)
         {
                 LOGGER.log(Level.INFO, "\n1)Update GPA \n2)Student details \n3)exit");
                 LOGGER.log(Level.INFO, "Student choose any one the option in the above ");
                 int select = sc.nextInt();
                 if (select == 1) {
-                    LOGGER.log(Level.INFO, "Enter your latest GPA: ");
-                    upgpa = sc.nextFloat();
-                    s.updategpa(upgpa);
+                    LOGGER.log(Level.INFO, "Enter your latest grade: ");
+                    upgrade = sc.next().charAt(0);
+                    s.updategpa(upgrade);
                 } else if (select == 2) {
-                    res = s.name;
-                    LOGGER.info(res);
-                    LOGGER.log(Level.INFO, "has a");
-                    LOGGER.log(Level.INFO, Float.toString(s.studentgpa()), s.studentgpa());
+                    LOGGER.info(s.studentdetails());
                 } else if (select == 3) {
                     break;
             }
